8ab2c00e09a8855785af30fbf6654719baaffb1f
Sumanth Kumar Meesala <meesalasumanth1@gmail.com>
2019-04-08 18:18:49 +1000

    react-dom added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
-import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "popper.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import Register from "./components/Register";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Router>
+    <React.Fragment>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/Register" component={Register} />
+    </React.Fragment>
+  </Router>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
