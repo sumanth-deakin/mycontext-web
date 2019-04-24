cdf8fbefdfcfc1cd8a798744e8b1b69c0bedf683
Sumanth Kumar Meesala <smeesala@deakin.edu.au>
2019-04-24 22:08:33 +1000

    Sidebar added

---
+++ src/components/SideBar.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import "../css/SideBar.css";
+import Icon from "@mdi/react";
+import { mdiHome, mdiFile } from "@mdi/js";
+
+class SideBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="sidebar-sticky">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <div className="box">
+              <Icon
+                path={mdiHome}
+                size={1}
+                horizontal
+                vertical
+                rotate={180}
+                color={this.props.current === "home" ? "" : "#9e9e9e"}
+              />
+              <a
+                className={
+                  "nav-link " + (this.props.current === "home" ? "active" : "")
+                }
+                href="/"
+              >
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </div>
+          </li>
+
+          <li className="nav-item">
+            <div className="box">
+              <Icon
+                path={mdiFile}
+                size={1}
+                horizontal
+                vertical
+                rotate={180}
+                color={this.props.current === "records" ? "" : "#9e9e9e"}
+              />
+              <a
+                className={
+                  "nav-link " +
+                  (this.props.current === "records" ? "active" : "")
+                }
+                href="/records"
+              >
+                Records
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
