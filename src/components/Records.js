103783bcc24c50b85673ae68f17f0e897e9cc7d0
Sumanth Kumar Meesala <smeesala@deakin.edu.au>
2019-05-13 14:00:51 +1000

    http to https changed

--- src/components/Records.js
+++ src/components/Records.js
@@ -28,7 +28,7 @@ class Records extends Component {
 
     var self = this;
 
-    var url = "http://api-mycontext.herokuapp.com/record/listOwnerRecords";
+    var url = "https://api-mycontext.herokuapp.com/record/listOwnerRecords";
 
     var payload = {
       token: localStorage.getItem("access-token"),
@@ -75,7 +75,7 @@ class Records extends Component {
       loading: true
     });
 
-    var url = "http://api-mycontext.herokuapp.com/record/deleteRecord";
+    var url = "https://api-mycontext.herokuapp.com/record/deleteRecord";
     var self = this;
 
     var payload = {
@@ -123,7 +123,7 @@ class Records extends Component {
       loading: true
     });
 
-    var url = "http://api-mycontext.herokuapp.com/record/changeOwnership";
+    var url = "https://api-mycontext.herokuapp.com/record/changeOwnership";
     var self = this;
 
     var payload = {
